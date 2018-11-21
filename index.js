27a90a0861148356b58204390342983b6ce66ed1
maxwellbenton <maxwellbenton@gmail.com>
2018-11-21 09:22:39 -0500

    adds solution

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const testVar = {}
+document.addEventListener( "DOMContentLoaded", function () {
+  fetchDeck()
+} )
 
-function testFunc() {
-  return "hi"
+function fetchDeck() {
+  fetch( 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1' )
+    .then( res => res.json() )
+    .then( json => {
+      setupGame( json )
+    } )
+}
+
+function setupGame( deck ) {
+  document.querySelector( '#deal' ).addEventListener( 'click', function () {
+    fetchCard( deck.deck_id )
+  } )
+}
+
+function fetchCard( deck_id ) {
+  fetch( `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1` )
+    .then( res => res.json() )
+    .then( json => {
+      addCardToDOM( json.cards[ 0 ] )
+    } )
+}
+
+function addCardToDOM( card ) {
+  let image = document.createElement( 'img' )
+  image.src = card.image
+  document.querySelector( 'section' ).appendChild( image )
 }
